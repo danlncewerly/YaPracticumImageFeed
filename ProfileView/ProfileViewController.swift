ee27597f62012caa3046ff1058128b8885f3313c
danlncewerly <146477252+danlncewerly@users.noreply.github.com>
2024-11-24 18:17:36 +0300

    Add files via upload

---
+++ ProfileView/ProfileViewController.swift
@@ -0,0 +1,90 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Photo")
+        imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.textColor = .ypWhite
+        label.font = UIFont.boldSystemFont(ofSize: 23.0)
+        return label
+    }()
+    
+    private let nickNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@ekaterina_nov"
+        label.textColor = .ypGrey
+        label.font = UIFont.systemFont(ofSize: 13.0)
+        return label
+    }()
+    
+    private let profileDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.textColor = .ypWhite
+        label.font = UIFont.systemFont(ofSize: 13.0)
+        return label
+    }()
+    
+    private var exitButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "ipad.and.arrow.forward")!, for: .normal)
+        button.addTarget(ProfileViewController.self, action: #selector(didTapExitProfileButton), for: .touchUpInside)
+        button.tintColor = .ypRed
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        makeConstraints()
+    }
+    
+    private func addSubviews() {
+        [
+            profileImageView,
+            nameLabel,
+            nickNameLabel,
+            profileDescriptionLabel,
+            exitButton
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    private func makeConstraints() {
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            
+            nickNameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            nickNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
+            profileDescriptionLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            profileDescriptionLabel.topAnchor.constraint(equalTo: nickNameLabel.bottomAnchor, constant: 8),
+            
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            exitButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor)
+        ])
+    }
+    
+    @objc
+    private func didTapExitProfileButton() {
+        
+    }
+}
+
