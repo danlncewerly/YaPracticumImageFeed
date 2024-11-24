221b75a54597ce992e4d21b17eae7847d469715c
danlncewerly <146477252+danlncewerly@users.noreply.github.com>
2024-11-24 18:16:31 +0300

    Add files via upload

---
+++ Auth/AuthViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        vc.dismiss(animated: true)
+        oAuth2Service.fetchOAuthToken(code: code) { [self] result in
+            switch result {
+            case .success(let token):
+                oAuth2TokenStorage.token=token
+                delegate?.didAuthenticate(self)
+            case .failure(_):
+                print("Authorization error")
+            }
+        }
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
+final class AuthViewController: UIViewController {
+    private let oAuth2Service = OAuth2Service.shared
+    private let oAuth2TokenStorage = OAuth2TokenStorage.shared
+    
+    var delegate: AuthViewControllerDelegate?
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+
---
+++ ImageList/ImagesListCell.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var gradientView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        gradient()
+    }
+    
+    private func gradient() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradient.locations = [0.03, 2.8, 1]
+        gradient.frame = gradientView.bounds
+        gradientView.layer.addSublayer(gradient)
+    }
+}
